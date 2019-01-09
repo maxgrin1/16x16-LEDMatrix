8a5098cb8eea8b4734abab7f6fc4d05e4ce268d1
maxvangrinsven <42255180+maxgrin1@users.noreply.github.com>
2019-01-09 09:18:21 +0100

    Translated into english

--- OLD_bluetooth control sketch.ino
+++ OLD_bluetooth control sketch.ino
@@ -1,5 +1,7 @@
+//This is an old sketch, will upload the updated one(for example with brightness adjustments) when I get to it.
+
 #include "FastLED.h"
-//'instellen' van Matrix
+//A bunch of variables
 #define DATA_PIN    6
 #define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
@@ -7,15 +9,15 @@
 #define MAXBRIGHTNESS 150
 CRGB leds[NUM_LEDS];
 #define SECONDS_PER_PALETTE 15
-int state = 0; //Geen idee waarom deze 0 moet zijn maar anders doet hij het niet.
+int state = 0;
 void setup() {
   delay(1000);
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setDither(MAXBRIGHTNESS < 255);
   FastLED.setBrightness(MAXBRIGHTNESS);
-  Serial.begin(9600); // Voor de communicatie met Bluetooth en debug
+  Serial.begin(9600); // Needed in order to use Bluetooth(mobile app), and for debug info
   debugInfo();
 }
-//Allemaal dingen voor de palletes
+//for the pallets from the fastled library
 extern const TProgmemRGBGradientPalettePtr gGradientPalettes[];
 extern const uint8_t gGradientPaletteCount;
 uint8_t gCurrentPaletteNumber = 0;
@@ -24,16 +26,16 @@ CRGBPalette16 gTargetPalette( gGradientPalettes[0] );
 
 
 void loop(){
-  //Check of er data is doorgegeven
+  //Check if there is data
   SerialCheck();
-//Als de loop hier komt heeft hij data(YEAAHHS).
+//if the loop gets here, we have data YEAHHHH
   if(state == 1){
     fill_solid(leds, NUM_LEDS, CRGB::Black);
     FastLED.show();
     SerialCheck();
   }
-  while(state == 2){ //Moet while zijn omdat het een animatie is dat moet blijven loopen
-  FastLED.clear(); //Zet alle leds uit anders kan je rare mixen gaan krijgen.
+  while(state == 2){ 
+  FastLED.clear(); //turn off all the LEDs
       EVERY_N_SECONDS( SECONDS_PER_PALETTE ) {
         gCurrentPaletteNumber = addmod8( gCurrentPaletteNumber, 1, gGradientPaletteCount);
         gTargetPalette = gGradientPalettes[ gCurrentPaletteNumber ];
@@ -44,7 +46,7 @@ void loop(){
       }
       colorwaves( leds, NUM_LEDS, gCurrentPalette);
       FastLED.show();
-      //Hier kan ik de felheid aanpassen, heeft continu effect
+      //Here you can adjust the brightness
       SerialCheck();
   }
   if(state == 3){
@@ -57,12 +59,12 @@ void loop(){
     FastLED.show();
     SerialCheck();
   }
-  if(state == 8){ //Plaats voor de felheid.
+  if(state == 8){ //Here comes brightness.
     
   }
 }
 
-//Check de Serial of er data beschikbaar is, zoja sla deze op in de 'state' variable.
+//Check if there is Serial data comming from the Bluetooth Module, if so store that data in the state variable.
 void SerialCheck(){
   if(Serial.available() > 0){
     state = Serial.read();
@@ -71,10 +73,10 @@ void SerialCheck(){
 }
 
 void debugInfo(){
-  Serial.println("Sketch gemaakt door Max");
+  Serial.println("Sketch made by Max van Grinsven");
   Serial.print("Sketch: ");
   Serial.println(__FILE__);
-  Serial.print("Datum: ");
+  Serial.print("Date: ");
   Serial.println(__DATE__);
 }
 
